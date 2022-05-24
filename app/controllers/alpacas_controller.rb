ef7095d92e36bd513e44bf8892db924fabf84ec9
Vincent Stuber <vincentstuber97@googlemail.com>
2022-05-24 17:28:23 +0200

    adding simple alpaca creation form

--- app/controllers/alpacas_controller.rb
+++ app/controllers/alpacas_controller.rb
@@ -1,5 +1,28 @@
 class AlpacasController < ApplicationController
+  COLORS = ["white", "gray", "brown", "black"]
+  WOOL_TYPES = ["Suri", "Huacaya"]
+
   def index
     @alpacas = Alpaca.all
+  end
+
+  def new
+    @alpaca = Alpaca.new
+  end
+
+  def create
+    @alpaca = Alpaca.new(alpaca_params)
+    @alpaca.owner = current_user
+    if @alpaca.save
+      redirect_to alpaca_path(@alpaca)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def alpaca_params
+    params.require(:alpaca).permit(:name, :nick_name, :age, :price_per_day, :height, :weight, :color, :wool_type)
   end
 end
